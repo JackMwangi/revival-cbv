f8d5ac4f2378a8b97f91123bc80463aa328ecfc0
Jack Mwangi <jack.wachira@andela.com>
2016-08-04 13:11:08 +0300

    Add fbv

---
+++ blog/articles/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+
+from .views import blog_home, article_detail, article_create, article_delete, \
+    article_update
+
+
+urlpatterns = [
+    url(r'^$', blog_home, name="blog-home"),
+    url(r'^(?P<pk>\d+)/$', article_detail, name="article-detail"),
+
+    url(r'^create/$', article_create, name="article-create"),
+    url(r'^delete/(?P<pk>\d+)/$', article_delete, name="article-delete"),
+    url(r'^update/(?P<pk>\d+)/$', article_update, name="article-update"),
+]
---
+++ blog/blog/urls.py
@@ -0,0 +1,12 @@
+
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from articles.views import blog_home
+
+
+urlpatterns = [
+    url(r'^$', blog_home),
+    url(r'^admin/', admin.site.urls),
+    url(r'^blog/', include("articles.urls")),
+]
